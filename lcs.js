0468eaf54c577091609702af93c46aa44bdb75a3
Logarithmus <logarithmus.dev@gmail.com>
2019-01-29 02:10:31 +0300

    140 bytes -> 139

--- lcs.js
+++ lcs.js
@@ -2,8 +2,8 @@ let str_arr = process.argv.slice(2);
 let lcs = "";
 // ternary has higher precedence than -=
 for (let i = 0, start = 0, end_margin = 0; i < str_arr.length;
-	        start += str_arr[i++].includes(
+	        start -= str_arr[i++].includes(
 	       	// end_margin <= 0 
-	            lcs = str_arr[0].substr(start, str_arr[0].length - end_margin)
-	          ) ? 0 : start > (i = 0) ? -1 : ++end_margin);
+	            lcs = str_arr[0].substr(start, str_arr[0].length + end_margin)
+	          ) ? 0 : start > (i = 0) ? 1 : --end_margin);
 console.log(lcs);
